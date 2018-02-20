65e4890dfef27881b27112be639ebea92cfe466f
Satya Pavan <pavan.simham@gmail.com>
2018-02-21 00:38:55 +0530

    with a working digital and analog

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+var intervalId = null;
+
 function getCurrentTime() {
 	console.log("Entering into getCurrentTime");
 
@@ -11,21 +13,73 @@ function getCurrentTime() {
 function paintDigital() {
 	console.log("Entering into paintDigital");
 	currTime = getCurrentTime();
-	document.getElementById("main_content").innerHTML = currTime.getHours() + ":" + currTime.getMinutes() + ":" + currTime.getSeconds();
+	document.getElementById("main_content").innerHTML = 
+	( currTime.getHours() 	>= 10 ? currTime.getHours()	 : "0" + currTime.getHours())
+	+ ":" + 
+	( currTime.getMinutes() >= 10 ? currTime.getMinutes() : "0" + currTime.getMinutes())
+	+ ":" + 
+	( currTime.getSeconds() >= 10 ? currTime.getSeconds() : "0" + currTime.getSeconds());
 }
 
 function paintAnalog() {
 	console.log("Entering into paintAnalog");
-	currTime = getCurrentTime();
-	document.getElementById("main_content").innerHTML = "Coming soon";
+
+	document.getElementById("main_content").innerHTML = '<canvas id="canvas" width="400" height="400"></canvas>';
+	
+	var canvas = document.getElementById("canvas");
+	var ctx = canvas.getContext("2d");
+	var radius = canvas.height / 2;
+	ctx.translate(radius, radius);
+	radius = radius * 0.90 ;
+
+	drawClock(ctx, radius);
+
 }
 
 window.addEventListener("DOMContentLoaded", function() {
-	document.getElementById("b_digital").addEventListener("click", function(){
-		setInterval(paintDigital, 1000);
+
+	document.getElementById("id_digital").addEventListener("click", function(){
+
+		// make the current one not active
+   		$("#navbarSupportedContent").find(".active").removeClass("active");
+
+   		// make the pressed/new one as active
+		$(this).addClass("active");
+
+		if ( intervalId !== null ) clearInterval(intervalId);
+
+		document.getElementById("main_content").classList.add("digital-time");
+
+		intervalId = setInterval(paintDigital, 1000);
+	});
+
+	document.getElementById("id_analog").addEventListener("click", function(){
+		
+		// make the current one not active
+   		$("#navbarSupportedContent").find(".active").removeClass("active");
+
+   		// make the pressed/new one as active
+		$(this).addClass("active");
+
+		if ( intervalId !== null ) clearInterval(intervalId);
+
+		document.getElementById("main_content").classList.add("analog-time");
+
+		intervalId = setInterval(paintAnalog, 1000);
 	} );
-	document.getElementById("b_analog").addEventListener("click", function(){
-		setInterval(paintAnalog, 1000);
+
+	document.getElementById("id_reverse").addEventListener("click", function(){
+
+		// make the current one not active
+   		$("#navbarSupportedContent").find(".active").removeClass("active");
+
+   		// make the pressed/new one as active
+		$(this).addClass("active");
+
+		if ( intervalId !== null ) clearInterval(intervalId);
+		intervalId = setInterval(paintReverseAnalog, 1000);
 	} );
+
+
     }, false);
 
